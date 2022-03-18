9f467bab76b8697b140dfded0d46ba019e108e06
LauraTDauden <84546617+LauraTDauden@users.noreply.github.com>
2022-03-18 15:53:51 +0000

    Initial commit

---
+++ src/main/java/com/bb2/goodsmanagement/GoodsManagementApplication.java
@@ -0,0 +1,14 @@
+package com.bb2.goodsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsManagementApplication.class, args);
+	}
+
+
+}
