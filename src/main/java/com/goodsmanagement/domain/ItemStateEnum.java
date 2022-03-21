8cbde91bc5ec4ab96de1368db0fb46100bc102eb
LauraTDauden <lauratdauden@gmail.com>
2022-03-21 10:40:19 +0000

    Updated initialzr

--- src/main/java/com/goodsmanagement/domain/ItemStateEnum.java
+++
@@ -1,6 +0,0 @@
-package com.goodsmanagement.domain;
-
-public enum ItemStateEnum {
-    ACTIVE,
-    DISCONTINUED
-}
--- src/main/java/com/goodsmanagement/domain/RoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.goodsmanagement.domain;
-
-public enum RoleEnum {
-    MEMBER,
-    ADMIN
-}
