ed511fcee200a848eb43847317069980d7b01249
LauraTDauden <lauratdauden@gmail.com>
2022-03-29 14:06:42 +0100

    Added PriceReduction Service interface

---
+++ src/main/java/com/bb2/goodsmanagement/service/interfaces/IPriceReductionService.java
@@ -0,0 +1,9 @@
+package com.bb2.goodsmanagement.service.interfaces;
+
+import com.bb2.goodsmanagement.domain.PriceReduction;
+
+import java.util.List;
+
+public interface IPriceReductionService {
+    public List<PriceReduction> priceReductionList ();
+}
